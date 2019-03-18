6b6bb52d63647636324222b2a0a1f49ec3977711
_alexandrsmirnov_ <2freaqq@gmail.com>
2019-03-18 17:55:25 +0300

    add Read Me

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css'
+import { withBookstoreService } from '../hoc'
+
+
+const App = ({ bookstoreService }) => {
+
+  console.log(bookstoreService);
+
+  return <div>App</div>
+
+};
+
+export default withBookstoreService(App);
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,9 @@
+
+
+export default class BookstoreService {
+
+  getbooks(){
+    return [];
+  }
+
+}
